3c44ff15f8cb4ff49053b809f67357bc12065d5f
salmanthecoder <rahilsalman@gmail.com>
2020-01-02 16:58:08 +0100

    added cors

--- src/main/java/com/consumerest/PlanetInfoController.java
+++ src/main/java/com/consumerest/PlanetInfoController.java
@@ -1,13 +1,16 @@
 package com.consumerest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
-public class PlanetInfoController {
+public class PlanetInfoController{
     @Autowired
     private PlanetInfoService planetInfoService;
+    @CrossOrigin
     @RequestMapping("/planet/info")
     public PlanetDetails getPlanetDetails() throws Exception {
         return planetInfoService.getPlanetDetalis();
